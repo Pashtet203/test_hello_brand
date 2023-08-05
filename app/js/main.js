d6949d4ff966be57ac89835c57c90c2f347266e7
Pavel <74721864+Pashtet203@users.noreply.github.com>
2023-08-05 18:14:42 +0400

    modal+ ajax

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
+//отправка формы
+$(function () {
+    $('.modal__form').on('submit',function(event){
+        event.preventDefault();
+        const data = {
+            num_person: $(".num_per"),
+            phone: $('tel')
+        }
+        $.ajax({
+            url: '#',
+            type: 'post',
+            data: data,
+            success: function(response){
+                console.log('success');
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    })
+})
 
+
+// слайдер
 const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: ".next",
@@ -6,7 +29,35 @@ const swiper = new Swiper('.swiper', {
     }
 })
 
+
+
+//модалка
 document.querySelector('.header__navbar-open').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__navbar-box').classList.toggle('open');
 })
+
+
+document.querySelector(".header__info-btn").addEventListener("click", function() {
+    document.querySelector(".modal").classList.add("openM")
+})
+
+document.querySelector(".modal_exit").addEventListener("click", function() {
+    document.querySelector(".modal").classList.remove("openM")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.querySelector(".modal").classList.remove("openM")
+    }
+});
+
+document.querySelector(".modal .modal__inner").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.querySelector(".modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('openM');
+});
+
+
